1a61b37a99e1bd176b8326d136d0f831576342f3
sergey_iagfarov <dedaz27@mail.ru>
2022-10-10 23:07:47 +0500

    задания

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,70 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
 
     }
 
-    public static void task1() {
+    public static void task4() {
+        int sum = 15000;
+        int monthnum = 1;
+        while (sum <= 12_000_000) {
+            sum *= 1.07;
+            System.out.println("Месяц " + monthnum + ", сумма накоплений составляет " + sum);
+            monthnum++;
+        }
+    }
+
+    public static void task5() {
+        int sum = 15000;
+        int monthnum = 1;
+        while (sum <= 12_000_000) {
+            sum *= 1.07;
+            if (monthnum % 6 == 0) {
+                System.out.println("Месяц " + monthnum + ", сумма накоплений составляет " + sum);
+            }
+            monthnum++;
+        }
+    }
+
+    public static void task6() {
+        int sum = 15000;
+        int monthnum = 1;
+        for (int i = 1; i < 9 * 12; i += 6) {
+            sum *= 1.07;
+            if (monthnum % 6 == 0) {
+                System.out.println("Месяц " + i + ", сумма " + sum);
+            }
+            monthnum++;
+        }
+    }
+
+    public static void task7() {
+        int firstFriday = 4;
+        for (int currentFriday = firstFriday; currentFriday <= 31; currentFriday += 7) {
+            System.out.println("Сегодня пятница, " + currentFriday + "-ое число " + "Необходимо подготовить отчет.");
+        }
+    }
+
+    public static void task8() {
+        int currentYear = 2022;
+        int beginning = currentYear - 200;
+        int ending = currentYear + 100;
+        for (int i = beginning; i < ending; i++) {
+            if (i % 79 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void task9() {
         int i = 1;
-        int salary = 15_000;
-        int total = 0;
-        while (total <= 2_459_000) {
-            total = total + total / 100;
-            total = total + salary;
-
-            System.out.println("Месяц " + i + ", сумма накопление " + total + " рублей");
-            i++;
-
-        }
-    }
-
-    public static void task2() {
-        int i = 1;
-        while (i <= 10) {
-            System.out.println(i++);
-        }
-        for (int a = 10; a >= 1; ) {
-            System.out.println(a--);
-        }
-    }
-
-    public static void task3() {
-        int population = 12_000_000;
-        int birthRate = 17 * population / 1000;
-        int deathRate = 9 * population / 1000;
-        for (int i = 1; i <= 10; i++) {
-            population += birthRate;
-            population -= deathRate;
-            System.out.println("Год " + i + ", численность населения составляет " + population);
-            // так же решала Ольга Лугачева в видеоразборе
+        for (; i <= 10; i++) {
+            System.out.println("2*" + i + " = " + 2 * i);
         }
     }
 }
